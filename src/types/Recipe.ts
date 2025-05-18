a8be26cb2cbbe6c4edf794a43b93eacb015370fb
sdsharma1469 <shaunak.sharma@gmail.com>
2025-05-17 22:45:17 -0700

    Recipe Type Conflicts Solved

--- src/types/Recipe.ts
+++ src/types/Recipe.ts
@@ -1,17 +1,20 @@
-// types/recipe.ts
-
-export interface Recipe {
-  createdAt : any
-  recipeId: string
-  name: string
-  ingredients: string[]
-  steps: string[]
-  halal: boolean
-  vegan: boolean
-  vegetarian: boolean
-  lactoseFree: boolean
-  rating: number
-  comments: string[]
-}
-
-  
+export type Recipe = {
+  recipeId: string;
+  name: string;
+  description?: string;
+  ingredients: Array<{
+    quantity: number;
+    name: string;
+  }>;
+  steps: string[];
+  tags: {
+    vegan: boolean;
+    vegetarian: boolean;
+    lactoseFree: boolean;
+    halal: boolean;
+    soy: boolean;
+    peanuts: boolean;
+  };
+  creatorRating: number;
+  difficulty: number;
+};
